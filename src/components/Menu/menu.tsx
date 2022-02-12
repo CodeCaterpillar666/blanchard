6d627dfed2089fd4407b7f18990c74f4061471ad
ziweiwang98 <wang.ziwei2@northeastern.edu>
2022-02-12 00:00:50 -0800

    Update Menu component and MenuItem component: 1.In menu.tsx, replace original bare {children} in return with a function that ensures all elements inside Menu component are MenuItem components; 2.In menu.tsx, use React.cloneeElement to auto-add index property into each child MenuItem component;

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,5 +1,6 @@
 import React, { useState, createContext } from "react";
 import classNames from "classnames";
+import { MenuItemProps } from "./menuItem"
 
 // typescript, string literal type
 type MenuMode = 'horizontal' | 'vertical';
@@ -37,11 +38,23 @@ const Menu: React.FC<MenuProps> = (props) => {
         index: currentActive? currentActive : 0,
         onSelect: handleClick,
     }
+    const renderChildern = () => {
+        return React.Children.map(children, (child, index) => {
+            const childElement = child as React.FunctionComponentElement<MenuItemProps>
+            const {displayName} = childElement.type
+            if (displayName === 'MenuItem') {
+                // use React.cloneElement to auto-add index property into each child component
+                return React.cloneElement(childElement, {index})
+            } else {
+                console.log('Warning: Menu has a child which is not a MenuItem component')
+            }
+        })
+    }
     return (
         <>
             <ul className={classes} style={style} data-testid="test-menu">
                 <MenuContext.Provider value={passedContext}>
-                    {children}
+                    {renderChildern()}
                 </MenuContext.Provider>
             </ul>
         </>
