fb3b78ac2039e89536c688e8dfbf23698f0c9ebe
ziweiwang98 <wang.ziwei2@northeastern.edu>
2022-02-20 23:30:10 -0800

    Add AutoComplete component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,11 @@
 // global imported styles
 import "../src/styles/index.scss";
 
+// loading icons
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
