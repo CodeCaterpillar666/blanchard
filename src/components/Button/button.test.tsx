966cd202ba92362fab8dbe39ffee72abcb05c429
ziweiwang98 <wang.ziwei2@northeastern.edu>
2022-02-24 15:13:30 -0800

    Update tests

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import Button, { ButtonProps, ButtonSize, ButtonType } from "./button";
 
 const defaultProps = {
@@ -17,11 +17,15 @@ const disabledProps: ButtonProps = {
     onClick: jest.fn()
 }
 
+// https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/prefer-screen-queries.md
+// Based on this rule, use screen to query.
+// "This rule aims to force writing tests using built-in queries directly from screen object rather than destructuring them from render result."
+
 describe('test Button component', () => {
     it('should render the correct default button', () => {
         // 1.test if element is in the dom
-        const wrapper = render(<Button {...defaultProps}>Nice</Button>);
-        const element = wrapper.getByText('Nice');
+        render(<Button {...defaultProps}>Nice</Button>);
+        const element = screen.getByText('Nice');
         // just-dom library添加测试方法，比toBeTruthy好理解多了，即判断element是否在dom内
         expect(element).toBeInTheDocument();
         expect(element.tagName).toEqual('BUTTON');
@@ -32,22 +36,22 @@ describe('test Button component', () => {
     })
     it('should render the correct component based on different props', () => {
         // 1.test if element is in the dom
-        const wrapper = render(<Button {...testProps}>Nice</Button>);
-        const element = wrapper.getByText('Nice');
+        render(<Button {...testProps}>Nice</Button>);
+        const element = screen.getByText('Nice');
         expect(element).toBeInTheDocument();
         expect(element).toHaveClass('btn-primary btn-lg cls');
     })
     it('should render a link when btnType equals to link and href is provided', () => {
-        const wrapper = render(<Button btnType={'link'} href="http://testhref.abc">Link</Button>)
-        const element = wrapper.getByText('Link') as HTMLButtonElement;
+        render(<Button btnType={'link'} href="http://testhref.abc">Link</Button>)
+        const element = screen.getByText('Link') as HTMLButtonElement;
         expect(element).toBeInTheDocument();
         expect(element.tagName).toEqual('A');
         expect(element).toHaveClass('btn btn-link');
         expect(element.disabled).toBeFalsy();
     })
     it('should render disabled button when disabled set to true', () => {
-        const wrapper = render(<Button {...disabledProps}>Disabled</Button>)
-        const element = wrapper.getByText('Disabled') as HTMLButtonElement;
+        render(<Button {...disabledProps}>Disabled</Button>)
+        const element = screen.getByText('Disabled') as HTMLButtonElement;
         expect(element).toBeInTheDocument();
         expect(element.disabled).toBeTruthy();
         fireEvent.click(element)
--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
-
+import { render, fireEvent, screen } from '@testing-library/react'
+// component to test
 import { Input, InputProps } from './input'
 
 const defaultProps: InputProps = {
@@ -9,8 +9,8 @@ const defaultProps: InputProps = {
 }
 describe('test Input component', () => {
   it('should render the correct default Input', () => {
-    const wrapper = render(<Input {...defaultProps}/>)
-    const testNode = wrapper.getByPlaceholderText('test-input') as HTMLInputElement
+    render(<Input {...defaultProps}/>)
+    const testNode = screen.getByPlaceholderText('test-input') as HTMLInputElement
     expect(testNode).toBeInTheDocument()
     expect(testNode).toHaveClass('input-inner')
     fireEvent.change(testNode, { target: { value: '23' } })
@@ -18,8 +18,8 @@ describe('test Input component', () => {
     expect(testNode.value).toEqual('23')
   })
   it('should render the disabled Input on disabled property', () => {
-    const wrapper = render(<Input disabled placeholder="disabled"/>)
-    const testNode = wrapper.getByPlaceholderText('disabled') as HTMLInputElement
+    render(<Input disabled placeholder="disabled"/>)
+    const testNode = screen.getByPlaceholderText('disabled') as HTMLInputElement
     expect(testNode.disabled).toBeTruthy()
   })
   it('should render different input sizes on size property', () => {
